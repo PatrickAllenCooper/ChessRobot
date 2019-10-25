f4e5f4f48df0b134a4fa32d2cf8554dc70834a0f
Patrick Allen Cooper <patrick.allen.cooper@gmail.com>
2019-10-25 15:50:50 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+####################
+# Patrick Cooper   #
+# Chess Playing AI #
+# 10-25-19          #
+####################
+
+
+# sets the state for a new game
+
+import chess
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci("C:/Users/patri/PycharmProjects/chess-ai/stockfish/Windows/stockfish_10_x64.exe")
+
+board = chess.Board()
+while not board.is_game_over():
+    result = engine.play(board, chess.engine.Limit(time=0.100))
+    board.push(result.move)
+
+engine.quit()
