27b093508d800e08ebd1220b06963526ee585e08
Patrick Allen Cooper <patrick.allen.cooper@gmail.com>
2019-12-24 22:27:18 -0700

    Documentation update.

--- main.py
+++ main.py
@@ -4,24 +4,40 @@
 # 10-25-19          #
 ####################
 
-
+################################
 # sets the state for a new game
+################################
+
 
 import chess.engine
 from boardvis import visboard
+from stockfish import Stockfish
 
-board = visboard.board
+#board = visboard.board
 
 
 def get_board_state():
     board = visboard.board
 
 
-engine = chess.engine.SimpleEngine.popen_uci("C:/Users/patri/PycharmProjects/chess-ai/stockfish/Windows/stockfish_10_x64.exe")
+# please use appropriate file path
+stockfish = Stockfish('C:/Users/patri/PycharmProjects/chess-ai/stockfish_10_x64.exe')
 
-board = chess.Board()
-while not board.is_game_over():
-    result = engine.play(board, chess.engine.Limit(time=0.100))
-    board.push(result.move)
+# set position by sequence of moves:
+stockfish.set_position(['e2e4', 'e7e6'])
 
-engine.quit()
+# set position by Forsyth–Edwards Notation (FEN):
+stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+
+print(stockfish.get_best_move()) # d2d4
+print(stockfish.is_move_correct('a2a3')) # True
+
+# get last move info:
+print(stockfish.info)
+# e.g. 'info depth 2 seldepth 3 multipv 1 score mate -1 nodes 11 nps 5500 tbhits 0 time 2 pv h2g1 h4g3'
+
+# set current engine's skill level:
+stockfish.set_skill_level(15)
+
+# get current engine's parameters:
+stockfish.get_parameters()
